94314ea90943d56ef2eb2a8c82d1bab0fc6d0a1c
Hunter Carmona <hcarmona@asu.edu>
2020-10-05 22:05:38 -0700

    Create AddingMachine.java

---
+++ Assignment1/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment02;
+/**
+ * Class that adds, subtracts, keeps history, clears memory, and converts operations to a string on an integer total
+ * @author Hunter Carmona
+ *
+ */
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /**
+  * Method to return the current total
+  * @return The current total
+  */
+  public int getTotal () {
+    return 0;
+  }
+  /**
+  * Method that adds an integer to the total
+  * @param value Integer to be added to the total
+  */
+  public void add (int value) {
+  }
+  /**
+  * Method that subtracts an integer from the total
+  * @param value Integer to subtract the total by
+  */
+  public void subtract (int value) {
+  }
+  /**
+  * Method that keeps a history of the transactions
+  * @return The history of the transactions
+  */
+  public String toString () {
+    return "";
+  }
+  /**
+  * Method that will clear the history of the transaction
+  */
+  public void clear() {
+  }
+}
