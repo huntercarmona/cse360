d5f9af45d801b50e21bd142b43420d8972064319
Hunter Carmona <hcarmona@asu.edu>
2020-10-05 22:12:26 -0700

    Update AddingMachine.java
    Completed methods

--- Assignment1/AddingMachine.java
+++ Assignment1/AddingMachine.java
@@ -6,39 +6,45 @@ package cse360assignment02;
  */
 public class AddingMachine {
   private int total;
-  
+  private String history = "";
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history += total;
   }
   /**
-  * Method to return the current total
-  * @return The current total
-  */
+   * Method to return the current total
+   * @return The current total
+   */
   public int getTotal () {
-    return 0;
+    return total;
   }
   /**
-  * Method that adds an integer to the total
-  * @param value Integer to be added to the total
-  */
+   * Method that adds an integer to the total
+   * @param value Integer to be added to the total
+   */
   public void add (int value) {
+	  history += " + " + value;
+	  total += value;
   }
   /**
-  * Method that subtracts an integer from the total
-  * @param value Integer to subtract the total by
-  */
+   * Method that subtracts an integer from the total
+   * @param value Integer to subtract the total by
+   */
   public void subtract (int value) {
+	  history += " - " + value;
+	  total -= value;
   }
   /**
-  * Method that keeps a history of the transactions
-  * @return The history of the transactions
-  */
+   * Method that keeps a history of the transactions
+   * @return The history of the transactions
+   */
   public String toString () {
-    return "";
+    return history;
   }
   /**
-  * Method that will clear the history of the transaction
-  */
+   * Method that will clear the history of the transaction
+   */
   public void clear() {
+	  history = "";
   }
 }
